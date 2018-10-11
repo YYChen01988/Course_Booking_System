d7a475a71ed7dd9cff7136211f420c9ed5e277c9
yingying <chen.yingying.01988@gmail.com>
2018-10-11 11:10:03 +0100

    booking and course classes set up

---
+++ src/main/java/com/example/codeclan/coursebookingsystem/CoursebookingsystemApplication.java
@@ -0,0 +1,12 @@
+package com.example.codeclan.coursebookingsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoursebookingsystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoursebookingsystemApplication.class, args);
+	}
+}
---
+++ src/main/java/com/example/codeclan/coursebookingsystem/models/Customer.java
@@ -0,0 +1,4 @@
+package com.example.codeclan.coursebookingsystem.models;
+
+public class Customer {
+}
